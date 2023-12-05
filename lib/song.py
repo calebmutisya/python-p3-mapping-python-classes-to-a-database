d7fdb0becdae3dddf4bb8b18f8f65a58d5b341d6
calebmutisya <mutisyacalebmusau@gmail.com>
2023-12-05 11:25:34 +0300

    Complete

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,9 @@
 from config import CONN, CURSOR
 
 class Song:
+
+    all=[]
+
     def __init__(self,name,album):
         self.id= None
         self.name=name
@@ -24,6 +27,7 @@ class Song:
         """
 
         CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
 
         self.id= CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
 
@@ -32,4 +36,30 @@ class Song:
         song=Song(name,album)
         song.save()
         return song
+    
+    @classmethod
+    def new_from_db(cls,row):
+        song= cls(row[1], row[2])
+        song.id= row[0]
 
+    def all(cls):
+        sql=""""
+            SELECT * FROM songs
+        """
+
+        all= CURSOR.execute(sql).fetchall()
+
+        cls.all=[cls.new_from_db(row) for row in all]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
